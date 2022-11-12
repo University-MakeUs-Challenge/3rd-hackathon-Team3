46116134350f172163dbc4910751d9d84283f0f2
잉퓨 <dayeon620@kakao.com>
2022-11-13 06:15:03 +0900

    feat: 명함 수정 기능 추가

--- Back-end/SpringBootTemplate/src/main/java/com/example/demo/src/card/CardController.java
+++ Back-end/SpringBootTemplate/src/main/java/com/example/demo/src/card/CardController.java
@@ -2,13 +2,14 @@ package com.example.demo.src.card;
 
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
-import com.example.demo.src.card.model.Card;
-import com.example.demo.src.card.model.GetCardReq;
-import com.example.demo.src.card.model.PatchCardReq;
-import com.example.demo.src.card.model.PostCardRes;
+import com.example.demo.src.card.model.*;
 
+import com.example.demo.src.user.model.GetUserRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.DayOfWeek;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cards")
@@ -32,12 +33,13 @@ public class CardController {
     // Body
     @ResponseBody
     @PostMapping("/register")    // POST 방식의 요청을 매핑하기 위한 어노테이션
-    public BaseResponse<PostCardRes> createUser(@RequestBody GetCardReq getCardReq) {
+    public BaseResponse<PostCardRes> createCard(@RequestBody GetCardReq getCardReq) {
 
         try {
             PostCardRes postCardRes = cardService.createCard(getCardReq);
             return new BaseResponse<>(postCardRes);
         } catch (BaseException exception) {
+            exception.printStackTrace();
             return new BaseResponse<>((exception.getStatus()));
         }
     }
@@ -50,17 +52,29 @@ public class CardController {
     @PatchMapping("/{idx}")
     public BaseResponse<String> updateCard(@PathVariable("idx") int idx, @RequestBody Card card) {
         try {
-            PatchCardReq patchCardReq = new PatchCardReq(idx, card.getName(),
-                    card.getProfile_img(),card.getIntro(),card.getCard_front_img(),
-                    card.getCard_back_img(),card.getAge(),card.getGroup(),
-                    card.getMbti(), card.getBirth(),card.getExtra_info(),
-                    card.getUpdated_at(), card.getStatus(), card.getIs_main(),
-                    card.getOwner_idx(), card.getMaker_idx());
+            PatchCardReq patchCardReq = new PatchCardReq(
+                    card.getName(),
+                    card.getProfile_img(),
+                    card.getIntro(),
+                    card.getCard_front_img(),
+                    card.getCard_back_img(),
+                    card.getAge(),
+                    card.getGroup(),
+                    card.getMbti(),
+                    card.getBirth(),
+                    card.getExtra_info(),
+                    card.getUpdated_at(),
+                    card.getStatus(),
+                    card.getIs_main(),
+                    card.getOwner_idx(),
+                    card.getMaker_idx(),
+                    idx);
             cardService.updateCard(patchCardReq);
 
             String result = "명함 정보가 수정되었습니다.";
             return new BaseResponse<>(result);
         } catch (BaseException exception) {
+            exception.printStackTrace();
             return new BaseResponse<>((exception.getStatus()));
         }
     }
