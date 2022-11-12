340ecfa3f38e72cd444f5d2776500f07695ea511
anonymousrecords <a.Records2022@gmail.com>
2022-11-13 02:37:37 +0900

    makecard 수정

--- Front-end/app/src/main/java/com/example/team_3/MakeCardFragment.kt
+++ Front-end/app/src/main/java/com/example/team_3/MakeCardFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.team_3.databinding.FragmentHomeBinding
 import com.example.team_3.databinding.FragmentMakecardBinding
