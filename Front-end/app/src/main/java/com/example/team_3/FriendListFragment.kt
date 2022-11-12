367b17c584ca638ad2516134753b819ea7b182d3
Yujin <jini1514@soongsil.ac.kr>
2022-11-13 05:25:30 +0900

    feat: friendListFragment 수정

--- Front-end/app/src/main/java/com/example/team_3/FriendListFragment.kt
+++ Front-end/app/src/main/java/com/example/team_3/FriendListFragment.kt
@@ -1,12 +1,14 @@
 package com.example.team_3
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.team_3.databinding.FragmentFriendlistBinding
 
 class FriendListFragment : Fragment() {
@@ -19,6 +21,8 @@ class FriendListFragment : Fragment() {
         viewBinding= FragmentFriendlistBinding.inflate(layoutInflater)
 
         val friendList: ArrayList<Friend> = arrayListOf()
+
+
 
         friendList.apply{
             add(Friend(R.drawable.sample, R.drawable.sample, "aera"))
@@ -41,6 +45,49 @@ class FriendListFragment : Fragment() {
 //        var gridlayoutManager = GridLayoutManager(context, 3,LinearLayoutManager.VERTICAL,true
         var gridlayoutManager = GridLayoutManager(context, 3)
         viewBinding.rvFriend.layoutManager = gridlayoutManager
+
+
+
+        friendAdapter.setItemClickListener(object: friendRVAdapter.OnItemClickListener{
+            fun onClick(v: View, data:Friend, position: Int) {
+                // 클릭 시 이벤트 작성
+//                Intent(this@FriendListFragment).apply{
+   //                 putExtra("name",name)
+ //                   putExtra("front",front)
+
+//                }
+
+                Toast.makeText(context,
+                    "${friendList[position].name}의 명함입니다.}",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onItemClick(v: View, data: Friend, position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                TODO("Not yet implemented")
+            }
+        })
+/*
+        profileAdapter.setOnItemClickListener(object : ProfileAdapter.OnItemClickListener{
+            override fun onItemClick(v: View, data: ProfileData, pos : Int) {
+                Intent(this@MainActivity, ProfileDetailActivity::class.java).apply {
+                    putExtra("data", data)
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }.run { startActivity(this) }
+            }
+
+        })
+*/
+
+
 
 //        var recyclerList=recycleGridView.apply{
 //            setHasFixedSize(true)
--- Front-end/app/src/main/java/com/example/team_3/friendRVAdapter.kt
+++ Front-end/app/src/main/java/com/example/team_3/friendRVAdapter.kt
@@ -1,19 +1,32 @@
 package com.example.team_3
 
-import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.team_3.databinding.ItemFriendcardBinding
 
 class friendRVAdapter(private val dataList: ArrayList<Friend>): RecyclerView.Adapter<friendRVAdapter.ViewHolder>() {
     inner class ViewHolder(private val viewBinding: ItemFriendcardBinding) :
         RecyclerView.ViewHolder(viewBinding.root) {
+
         fun bind(position: Int) {
             viewBinding.imgFricard.setImageResource(dataList[position].front)
         }
+    }
+
+    interface OnItemClickListener : AdapterView.OnItemClickListener {
+        //fun onClick(v: View, position: Int)
+        fun onItemClick(v:View,data:Friend,position: Int)
+        //override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+          //  TODO("Not yet implemented")
+        //}
+    }
+
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,4 +40,40 @@ class friendRVAdapter(private val dataList: ArrayList<Friend>): RecyclerView.Ada
     }
 
     override fun getItemCount(): Int = dataList.size
+
+    fun setItemClickListener(onItemClickListener: Any) {
+        this.itemClickListener = onItemClickListener as AdapterView.OnItemClickListener
+    }
+    private lateinit var itemClickListener : AdapterView.OnItemClickListener
+
 }
+/*
+class myAdapter(val listener : OnItemClickListener) :  RecyclerView.Adapter<friendRVAdapter.ViewHolder>() {
+    interface OnItemClickListener {
+        fun onItemClick(v:View, position: Int)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        holder.front.setOnClickListener {
+            listener.onItemClick(holder.itemView, position)
+        }
+    }
+}*/
+
+/*
+public class friendRVAdapter extends RecyclerView.Adapter<PhRecyclerViewHolder> {
+
+    @Override
+    public PhRecyclerViewHolder onCreateViewHolder(ViewGroup a_viewGroup, int a_viewType) {
+        View view = LayoutInflater.from(a_viewGroup.getContext()).inflate(R.layout.content_recycler_item, a_viewGroup, false);
+        return new PhRecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PhRecyclerViewHolder a_viewHolder, int a_position) {
+        final PhRecyclerItem item = mItemList.get(a_position);
+
+        a_viewHolder.ivIcon.setImageResource(item.getImageResId());
+        a_viewHolder.tvName.setText(item.getName());
+    }
+*/
